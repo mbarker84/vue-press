f9d6fdfd2787e36b72370cb885e60e9a71fa2a29
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-02-10 22:07:44 +0000

    Setting up

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,29 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        vue-press
-      </h1>
-      <h2 class="subtitle">
-        My wonderful Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="posts">
+    <main>
+      <h2>Posts</h2>
+      <!-- here we loop through the posts -->
+      <div class="post" v-for="post in posts" :key="post.id">
+        <h3>
+          <!-- for each one of them, we’ll render their title, and link off to their individual page -->
+          <a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
+        </h3>
+        <div v-html="post.excerpt.rendered"></div>
+        <a :href="`blog/${post.slug}`" class="readmore">Read more ⟶</a>
       </div>
-    </div>
+    </main>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  computed: {
+    posts() {
+      return this.$store.state.posts
+    }
+  },
+  created() {
+    this.$store.dispatch('getPosts')
   }
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ public/wp-content/themes/vuetheme/index.php
@@ -0,0 +1,6 @@
+<meta content="0; URL='https://compassionate-fermi-26d2df.netlify.com''" http-equiv"refresh">
+
+<!-- just in case the meta tag is not read properly, here is plan B: a JS redirect -->
+<script type="text/javascript">
+  window.location = 'https://compassionate-fermi-26d2df.netlify.com';
+</script>
