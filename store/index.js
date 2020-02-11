9e7d62918cb3050319b26fb76a840a93079acac0
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-02-11 10:56:07 +0000

    Get posts from API

--- store/index.js
+++ store/index.js
@@ -2,12 +2,6 @@ export const state = () => ({
   posts: []
 })
 
-// export const getters = {
-//   getterValue: (state) => {
-//     return state.value
-//   }
-// }
-
 export const mutations = {
   updatePosts: (state, posts) => {
     state.posts = posts
@@ -19,7 +13,7 @@ export const actions = {
     if (state.posts.length) return
     try {
       let posts = await fetch(
-        `https://css-tricks.com/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
+        `${process.env.dbUrl}/?rest_route=/wp/v2/posts`
       ).then((res) => res.json())
       posts = posts
         .filter((el) => el.status === 'publish')
